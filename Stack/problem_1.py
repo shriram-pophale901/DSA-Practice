295c2e83aedd1c46f5b6edd10e5de88bebd48957
@shriram-pophale901 <shrirampophale901@gmail.com>
2026-05-19 21:34:14 +0530

    Add Stack problems

---
+++ Stack/problem_1.py
@@ -0,0 +1,89 @@
+'''
+7. Valid Parentheses (Stack Based) 
+Problem: Given a string containing just the characters (, ), {, }, [ and ], determine if the 
+input string is valid. 
+    • Input Format: A string. 
+    • Output Format: true or false. 
+    • Example: 
+    o Input: ()[]{} 
+    o Output: true 
+• Test Cases: 
+1. Input: (] → Output: false 
+2. Input: ([)] → Output: false 
+3. Input: {[]} → Output: true 
+
+'''
+
+
+# s = input()
+
+# stack = []
+
+# for ch in s:
+
+#     # opening brackets
+#     if ch in "([{":
+#         stack.append(ch)
+
+#     else:
+
+#         # stack empty
+#         if not stack:
+#             print("false")
+#             break
+
+#         top = stack.pop()
+
+#         # matching check
+#         if (ch == ")" and top != "(") or \
+#            (ch == "]" and top != "[") or \
+#            (ch == "}" and top != "{"):
+
+#             print("false")
+#             break
+
+# else:
+    
+#     if not stack:
+#         print("true")
+#     else:
+#         print("false")
+
+
+
+
+
+
+
+
+
+
+
+
+stack = []
+
+# PUSH
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("After push:", stack)
+
+
+# PEEK (top element)
+print("Top element:", stack[-1])
+
+
+# POP
+removed = stack.pop()
+
+print("Removed element:", removed)
+
+print("After pop:", stack)
+
+
+# isEmpty
+if not stack:
+    print("Stack is empty")
+else:
+    print("Stack is not empty")
